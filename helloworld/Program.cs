41562956d0b212a821e7c754cf43512359064403
conbutter <145612052+conbutter@users.noreply.github.com>
2025-04-23 14:33:48 -0600

    Created core baseline for HelloWorld program.

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    private static string hello_world = "Hello, World!";
+    private static bool program_active = true;
+
+    static void Main(string[] args)
+    {   
+        Console.WriteLine(hello_world + "\n");
+        DisplayMenu();
+
+        while ( program_active )
+        {
+            string user_input = Console.ReadLine();
+            if ( user_input == "1" )
+            {
+                Console.WriteLine(hello_world + "\n");
+            }
+            else if ( user_input == "2" )
+            {
+                // Functionality here
+            }
+            else if ( user_input == "3" )
+            {
+                program_active = false;
+            }
+            else
+            {
+                Console.WriteLine("This input is invalid; please choose an acceptable option.");
+            }
+        }
+    }
+
+    static void DisplayMenu()
+    {
+        Console.WriteLine("MAIN MENU\n1 - Print text\n2 - Customize text\n3 - Exit program\n\nSelection: ");
+    }
+}
--- helloworld/obj/Debug/net8.0/helloworld.AssemblyInfo.cs
+++ helloworld/obj/Debug/net8.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d499fdf9b3dbfa24b4d498992b4e356d05645541")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
